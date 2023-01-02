5d3a6f4b50f722b81eb0d327bc70924d5b1814d6
madhuresh <minoshideveloper8@gmail.com>
2023-01-02 18:54:32 +0530

    lec 12 cont...

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -2,6 +2,22 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
+//textures
+
+const textureLoader = new THREE.TextureLoader()
+
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+
 /**
  * Base
  */
@@ -10,6 +26,29 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+//object
+const material = new THREE.MeshBasicMaterial({color:0xf32345});
+
+const sphere =  new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    material
+)
+sphere.position.x = -1.5
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1,1),
+    material
+)
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry(0.5,0.2,16,32),
+    material
+)
+torus.position.x = 1.5
+
+scene.add(sphere,plane, torus)
+// scene.add(plane)
 
 /**
  * Sizes
@@ -65,6 +104,15 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    //upfate object
+    sphere.rotation.y  = elapsedTime * 0.2
+    plane.rotation.y  = elapsedTime * 0.2
+    torus.rotation.y  = elapsedTime * 0.2
+    sphere.rotation.x  = elapsedTime * 0.4
+    plane.rotation.x  = elapsedTime * 0.4
+    torus.rotation.x  = elapsedTime * 0.4
+
 
     // Update controls
     controls.update()
