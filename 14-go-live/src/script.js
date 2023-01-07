eb396c17d7c6f094af77a3cb199dd64300d0c0bf
madhuresh <minoshideveloper8@gmail.com>
2023-01-07 23:34:16 +0530

    chande

--- 14-go-live/src/script.js
+++ 14-go-live/src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'MINOSHI DEV',
             {
                 font: font,
                 size: 0.5,
