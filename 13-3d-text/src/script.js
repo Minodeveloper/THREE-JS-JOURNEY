82e29c19c840be28a1892acfa856dbb190b3a4dd
madhuresh <minoshideveloper8@gmail.com>
2023-01-07 23:32:17 +0530

    lec 14

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -19,14 +19,14 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 //axis helper
-const axisHelper = new THREE.AxesHelper()
-scene.add(axisHelper)
+// const axisHelper = new THREE.AxesHelper()
+// scene.add(axisHelper)
 
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
 
 /**
  * Fonts
