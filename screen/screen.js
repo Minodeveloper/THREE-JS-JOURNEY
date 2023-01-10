b88213fb0640dd56fffc43789db0c6447a4cc0e4
madhuresh <minoshideveloper8@gmail.com>
2023-01-11 00:09:00 +0530

    progress in making screen

--- screen/screen.js
+++ screen/screen.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const bodys = document.querySelector('body');
+const row0 = document.querySelector('#one')
+
+for(let i = 0; i < 90; i++)
+{
+    bodys.insertAdjacentHTML('beforeend', `<div class="row0"></div>`)
+    // console.log(row0)
+}
+
+for(let i = 0; i < 120; i++)
+{
+    row0.insertAdjacentHTML('beforeend',`<div class="pixel"></div>`)
+}
