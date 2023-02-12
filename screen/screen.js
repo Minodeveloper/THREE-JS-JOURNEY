2535f0023cee7d63ba46e930ad732dfbba31699a
madhuresh <minoshideveloper8@gmail.com>
2023-02-12 15:15:26 +0530

    screen design improved

--- screen/screen.js
+++ screen/screen.js
@@ -1,13 +1,27 @@
-const bodys = document.querySelector('body');
-const row0 = document.querySelector('#one')
+// const bodys = document.querySelector('body');
+// const row0 = document.querySelector('#one')
 
-for(let i = 0; i < 90; i++)
-{
-    bodys.insertAdjacentHTML('beforeend', `<div class="row0"></div>`)
-    // console.log(row0)
-}
+// for(let i = 0; i < 90; i++)
+// {
+//     bodys.insertAdjacentHTML('beforeend', `<div class="row0"></div>`)
+//     // console.log(row0)
+// }
 
-for(let i = 0; i < 120; i++)
-{
-    row0.insertAdjacentHTML('beforeend',`<div class="pixel"></div>`)
-}
+// for(let i = 0; i < 120; i++)
+// {
+//     row0.insertAdjacentHTML('beforeend',`<div class="pixel"></div>`)
+// }
+
+let mypix = document.querySelector(`#_${11}`);
+mypix.st
+
+let i = 1
+
+// setInterval(() => {
+//     mypix.style.gridArea = `1 / ${i} / 2 / ${i+1}`
+//         i++
+// }, 2000);
+
+// function setPixel (x_cordinate, y_cordinate){
+
+// }
