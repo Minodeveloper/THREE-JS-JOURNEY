68607cb7b4346cd4fa571ac60986f29462217380
madhuresh <minoshideveloper8@gmail.com>
2023-02-13 23:41:18 +0530

    progress

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -34,6 +34,24 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/nz.png'
 ])
 
+// 
+// PHYSICS
+// 
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+
+//sphere
+
+const sphereShape = new CANNON.Sphere(0.5)
+const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0,3,0),
+    shape: sphereShape
+})
+
+world.addBody(sphereBody)
+
+
 /**
  * Test sphere
  */
